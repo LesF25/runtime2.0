0dca199ab3d5d8824f1471f6ad1e11eaa7f8c919
lesf25 <dmitry.leschev@improvado.io>
2026-01-18 14:15:16 +0700

    refactor: request.cookie

--- sources/request/cookies.py
+++ sources/request/cookies.py
@@ -1,36 +1,16 @@
-from __future__ import absolute_import
+from collections import UserDict
 
-from builtins import str
-import sys
 
-from .headers import VDOM_dictionary
+class VDOM_cookies(UserDict[str, str]):
+	def __init__(
+		self,
+		data: dict | None = None,
+	) -> None:
+		super().__init__()
+		self.update(data or {})
 
-class VDOM_cookies(VDOM_dictionary):
-	"""Server cookies"""
-
-	def __init__(self, headers):
-		""" Constructor """
-		self.dict = {}
-		if "cookie" in headers:
-			cookie_list = headers["cookie"].split(';')
-			for item in cookie_list:
-				items = item.split('=')
-				if len(items) == 2:
-					self.dict[str(items[0]).strip().lower()] = str(items[1]).strip()
-
-	def cookies(self, cookies=None):
-		"""access cookies dictionary"""
-		return self.dict
-
-	def cookie(self, name, value=None, push=True):
-		"""read or push/add cookie"""
-		return self.value(name, value, push)
-
-	def get_string(self):
-		"""convert dictionary to the header string"""
-		#return 
-		string = ""
-		for c in list(self.dict.keys()):
-			if string != "": string += "; "
-			string += ("%s=%s" % (c, self.dict[c]))
-		return string
+	def __str__(self) -> str:
+		return '; '.join(
+			f'{k}={v}'
+			for k, v in self.data.items()
+		)
