8bae3f5f80da027bcdab5c5ec02a2c31e3dc2c46
lesf25 <dmitry.leschev@improvado.io>
2026-01-18 15:33:21 +0700

    refactor: request.arguments

--- sources/request/arguments.py
+++ sources/request/arguments.py
@@ -1,12 +1,7 @@
+from collections import UserDict
+from typing import Any
 
 
 # TODO: Redundant class. Remove it.
-class VDOM_request_arguments:
-    def __init__(self, args):
-        self.__arguments = args
-
-    def arguments(self, args=None):
-        if args:
-            self.__arguments = args
-
-        return self.__arguments
+class VDOM_request_arguments(UserDict[str, Any]):
+    ...
