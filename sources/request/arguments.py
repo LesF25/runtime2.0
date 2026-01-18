12eaedc62b503d81e3772135d4d07b4e8c92c7e1
lesf25 <dmitry.leschev@improvado.io>
2026-01-18 14:24:06 +0700

    refactor: Add TODO to request.arguments

--- sources/request/arguments.py
+++ sources/request/arguments.py
@@ -1,14 +1,12 @@
 
-from builtins import object
-class VDOM_request_arguments(object):
-    """request arguments"""
 
+# TODO: Redundant class. Remove it.
+class VDOM_request_arguments:
     def __init__(self, args):
-        """ Constructor """
         self.__arguments = args
 
     def arguments(self, args=None):
-        """access request property"""
         if args:
             self.__arguments = args
+
         return self.__arguments
